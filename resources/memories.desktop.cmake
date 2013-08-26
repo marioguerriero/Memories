9edfd1b9c8c9c4f8e304513693e71ed58d47c55d
Mefrio <mefrio.g@gmail.com>
2013-08-26 18:49:59 +0200

    added icon

--- resources/memories.desktop.cmake
+++ resources/memories.desktop.cmake
@@ -2,7 +2,7 @@
 Name=Memories
 Comment=Keep track of your best moments with your hands
 Exec=export APP_ID=memories && /usr/bin/qmlscene -I @MODULES_DIR@ @CMAKE_INSTALL_PREFIX@/share/memories/qml/memories.qml
-Icon=qmlscene
+Icon=@CMAKE_INSTALL_PREFIX@/share/memories/resources/icons/Memories.svg
 Terminal=false
 Type=Application
 X-Ubuntu-Touch=true
